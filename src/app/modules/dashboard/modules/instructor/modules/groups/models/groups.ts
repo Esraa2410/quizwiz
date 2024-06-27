a4ca9471e290cfb2a2adc8a48511174b8a20d5b3
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-06-27 20:04:11 +0300

    updating style/ service

--- src/app/modules/dashboard/modules/instructor/modules/groups/models/groups.ts
+++ src/app/modules/dashboard/modules/instructor/modules/groups/models/groups.ts
@@ -35,3 +35,11 @@ export interface IUdateGroupRes {
   message: string
 }
 
+export interface IStudentWithoutGroup{
+  _id: string
+  first_name: string
+  last_name: string
+  email: string ,
+  status:string,
+  role :string
+}
