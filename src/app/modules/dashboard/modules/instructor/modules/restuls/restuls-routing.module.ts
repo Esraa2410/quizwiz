aa5285b20efaa979c29dd1a8cdebcaae112e4e27
IbrahimShafiq4 <hima.shafiq@gmail.com>
2024-06-30 04:47:19 +0300

    results routing updated

--- src/app/modules/dashboard/modules/instructor/modules/restuls/restuls-routing.module.ts
+++ src/app/modules/dashboard/modules/instructor/modules/restuls/restuls-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RestulsComponent } from './restuls.component';
+import { ResultsViewComponent } from './components/results-view/results-view.component';
 
-const routes: Routes = [{ path: '', component: RestulsComponent }];
+const routes: Routes = [
+  { path: '', component: RestulsComponent, children: [
+    { path: 'results-view', component: ResultsViewComponent, title: 'Results View' }
+  ] }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
