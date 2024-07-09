8bf61438bdb1139edd7c5ad44caf90d43b362865
Esraa2410 <esraaaragabb@gmail.com>
2024-07-09 11:49:51 -0700

    change in student home

--- src/app/modules/dashboard/components/home/home.component.ts
+++ src/app/modules/dashboard/components/home/home.component.ts
@@ -18,82 +18,28 @@ export class HomeComponent implements OnInit {
   RouterLinkPath = '/dashboard/instructor/quizzes/view-Quiz';
   RouterLinkPath2 = '/dashboard/instructor/students/view-Quiz';
   loggedInRole: string = localStorage.getItem('role') ?? '';
-  upcomingQuizzesStudent = [];
-  completedQuizes = [];
-  data: any;
-  options: any;
-  userName:any;
-  email:any;
-  role:any;
+  userName: any;
+  email: any;
+  role: any;
 
 
-  constructor(
-    private _StudentQuizService:StudentQuizService,
-    private homeService: HomeService,
-    private _Router: Router,
-    private dialog: MatDialog
-  ) { this.getCompletedQuizesStud();
-    this.upComingQuizesStud();}
+  constructor(private _StudentQuizService: StudentQuizService, private homeService: HomeService, private _Router: Router, private dialog: MatDialog) {
+
+  }
 
   ngOnInit(): void {
     this.upComingExams();
     this.topStudents();
-    this.userName=localStorage.getItem('userName');
-    this.role=localStorage.getItem('role');
-    this.email=localStorage.getItem('email');
-   
+    this.userName = localStorage.getItem('userName');
+    this.role = localStorage.getItem('role');
+    this.email = localStorage.getItem('email');
+
   }
 
-  chart(){
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue('--text-color');
 
-    console.log(this.completedQuizes.length, this.upcomingQuizzesStudent.length)
-    this.data = {
-        labels: ['Completed Quizes', 'Upcoming Quizzes'],
-        datasets: [
-            {
-                data: [this.completedQuizes.length, this.upcomingQuizzesStudent.length],
-                backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
-                hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
-            }
-        ]
-    };
 
-    this.options = {
-        plugins: {
-            legend: {
-                labels: {
-                    usePointStyle: false,
-                    color: textColor
-                }
-            }
-        }
-    };
-  }
-  
-  upComingQuizesStud(): void {
-    this._StudentQuizService.getIcomingQuizes().subscribe({
-      next:(res)=>{
-        this.completedQuizes = res;
-      },complete:()=>{
-        this.chart()
-      }
-    })
-   
-  }
+ 
 
-  getCompletedQuizesStud(): void {
-    this._StudentQuizService.getCompletedQuizes().subscribe({
-      next:(res)=>{
-        this.upcomingQuizzesStudent = res;
-      },complete:()=>{
-        this.chart()
-      }
-       
-    })
-   
-  }
 
   upComingExams(): void {
     this.homeService.upComingFive().subscribe((quizzes) => {
